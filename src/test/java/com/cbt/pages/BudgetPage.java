a2b5fcf3e569d732aa5a7b11dfc0347ce115cb22
Aizada <aizadaboposhova@gmail.com>
2018-08-02 21:00:34 -0500

    Added Aizada's test cases

--- src/test/java/com/cbt/pages/BudgetPage.java
+++ src/test/java/com/cbt/pages/BudgetPage.java
@@ -1,5 +1,7 @@
 package com.cbt.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.cbt.utilities.Driver;
@@ -10,4 +12,34 @@ public class BudgetPage {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
 
+	@FindBy(xpath = "(//div[@id='nav']/ul//li)[3]")
+	public WebElement budget;
+	
+	@FindBy(xpath= "//*[contains(text(),'Expenses')]")
+	public WebElement expenses;
+	
+	@FindBy(xpath = "//div[@id=\"add_budget_item_header\"]/a")
+	public WebElement greenBtn;
+	
+	@FindBy(id = "TransactionCategoryName")
+	public WebElement category;
+	
+	@FindBy(id = "TransactionCategoryMonthlyBudget")
+	public WebElement budgetAmount;
+	
+	@FindBy(xpath = "//form[@id='budget_form_add_transaction_category']/ul[2]//button")
+	public WebElement submitBtn;
+	
+	@FindBy(xpath = "(//input[@id='Budget0Amount'])[1]")
+	public WebElement adjustment;
+	
+	@FindBy(xpath = "//span[@class='summary']/strong")
+	public WebElement totalBudget;
+	
+	@FindBy(xpath = "//li[@class='first']/a")
+	public WebElement dashboard;
+	
+	@FindBy(xpath = "//*[contains(text(),'$800.00')]")
+	public WebElement VerifyBudgeted;
+	
 }
